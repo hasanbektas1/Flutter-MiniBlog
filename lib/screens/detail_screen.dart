7915ef2e83d7c88d6e0e6b70cfb6fb6cbca74aab
hasanbektaş1 <hasanbktas1@gmail.com>
2023-12-22 19:35:09 +0300

    miniblog start - list/add/detail screens created

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,98 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:flutter/material.dart';
+import 'package:miniblog/models/blog.dart';
+
+class DetailScreen extends StatefulWidget {
+  const DetailScreen({Key? key, required this.blogId}) : super(key: key);
+
+  final String blogId;
+
+  @override
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Map<String, dynamic> blogs = {};
+  late Blog blogItem;
+
+  // https://tobetoapi.halitkalayci.com/api/Articles/17d1549f-3220-48bb-9ee8-08dc01eef2df
+  // https://tobetoapi.halitkalayci.com/api/Articles
+
+  fetchBlogsItem() async {
+    Uri url = Uri.parse(
+        "https://tobetoapi.halitkalayci.com/api/Articles/${widget.blogId}");
+    final response = await http.get(url);
+    final jsonData = json.decode(response.body);
+
+    setState(() {
+      blogs = jsonData;
+      //blog = Blog.fromJson(jsonData);
+      // blogs = Blog.fromJson(jsonData);
+      blogItem = Blog.fromJson(blogs);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchBlogsItem();
+    blogItem = Blog();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Blog Detay"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            if (blogItem.thumbnail != null)
+              AspectRatio(
+                aspectRatio: 16 / 9,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(
+                    blogItem.thumbnail.toString(),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            const SizedBox(height: 16),
+            Text(
+              blogItem.title.toString(),
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              blogItem.content.toString(),
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              "Yazar: ${blogItem.author.toString()}",
+              style: const TextStyle(
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                // Seçilen veri silme işlemi yapılacak
+              },
+              child: const Text("Sil"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:miniblog/models/blog.dart';
+import 'package:miniblog/screens/add_blog.dart';
+import 'package:miniblog/widgets/blog_item.dart';
+import 'package:http/http.dart' as http;
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  List<Blog> blogs = [];
+  List<Blog> reversedBlogs = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchBlogs();
+  }
+
+  fetchBlogs() async {
+    Uri url = Uri.parse("https://tobetoapi.halitkalayci.com/api/Articles");
+    final response = await http.get(url);
+    final List jsonData = json.decode(response.body);
+
+    setState(() {
+      blogs = jsonData.map((json) => Blog.fromJson(json)).toList();
+      reversedBlogs = blogs.reversed.toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Bloglar"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (ctx) => AddBlog()));
+                },
+                icon: const Icon(Icons.add))
+          ],
+        ),
+        body: reversedBlogs.isEmpty
+            ? const Center(child: CircularProgressIndicator())
+            : RefreshIndicator(
+                onRefresh: () async {
+                  fetchBlogs();
+                },
+                child: ListView.builder(
+                    itemBuilder: (context, index) =>
+                        BlogItem(blog: reversedBlogs[index]),
+                    itemCount: reversedBlogs.length),
+              ));
+  }
+}
